fe04c36224ef6ad68a0da8723938a2af3fe09b18
Wyman, Greg <greg.wyman@cbsinteractive.com>
2020-07-16 13:03:06 +0000

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,11 @@
 
 if [[ -z "${ENVIRONMENT}" ]]; then
   if [[ -z "${CODEBUILD_WEBHOOK_TRIGGER}" ]]; then
-    STAGE="dev"
+    STAGE="qa"
   else
     STAGE="${CODEBUILD_WEBHOOK_TRIGGER}"
   fi
 else
   STAGE="${ENVIRONMENT}"
 fi
-pwd
-ls -ltrh
-serverless deploy --stage $STAGE --verbose --aws-s3-accelerate --config ../serverless.yml
+serverless deploy --stage $STAGE --verbose --aws-s3-accelerate
